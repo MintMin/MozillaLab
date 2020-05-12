1eeb3e9e90417c761ebd509ee310839ddeadfe31
Qianti Min <qiamin@students.pitzer.edu>
2020-05-12 10:02:31 +0800

    the event list is never generated, so the web shows that there is no events.....

--- source/main/views.py
+++ source/main/views.py
@@ -6,3 +6,37 @@ class IndexPageView(TemplateView):
 
 class ChangeLanguageView(TemplateView):
     template_name = 'main/change_language.html'
+
+
+from django.shortcuts import render
+from search.models import Event
+from django.views.generic import ListView
+
+# def index(request):
+#     """View function for home page of site."""
+
+#     # Generate counts of some of the main objects
+#     events = Event.objects.all()
+#     # num_instances = BookInstance.objects.all().count()
+    
+#     # Available books (status = 'a')
+#     # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+#     # # The 'all()' is implied by default.    
+#     # num_authors = Author.objects.count()
+    
+#     context = {
+#         'events': events,
+        
+#     }
+
+    
+#     # Render the HTML template index.html with the data in the context variable
+#     return render(request, 'index.html', context=context)
+
+
+class EventList(ListView):
+        model = Event
+        context_object_name = 'event_list'   # your own name for the list as a template variable
+        queryset = Event.objects.all()  # Get 5 books containing the title war
+        template_name = 'main/index.html'  # Specify your own template name/location
--- source/search/urls.py
+++ source/search/urls.py
@@ -9,6 +9,5 @@ app_name = "search"
 
 urlpatterns = [
     path('', RunSearch.as_view(), name='run_search'),
-    path('', EventList.as_view())
    
 ]
