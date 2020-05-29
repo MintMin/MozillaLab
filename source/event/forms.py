53e86a14062542a383623a85f94600ad3b86b109
ndecavel <nbueff@students.pitzer.edu>
2020-05-28 18:05:52 -0700

    Small changes

--- source/event/forms.py
+++ source/event/forms.py
@@ -21,10 +21,6 @@ class CreateEventForm(forms.ModelForm):
 	years_to_display = range(datetime.datetime.now().year,
 	datetime.datetime.now().year + 2)
 	date = forms.DateField(widget=SelectDateWidget(years=years_to_display))
-	# main_recruiter = forms.CharField(label = 'Main Recruiter', max_length=40, required=True)
 	time = forms.TimeField(
         widget=SelectTimeWidget()
     )
-	# time = forms.TimeField(
- #        widget=SelectTimeWidget(dashed=True, race_time=True)
- #    )
--- source/event/urls.py
+++ source/event/urls.py
@@ -15,7 +15,6 @@ app_name = "event"
 
 urlpatterns = [
     path('', EventDashboard.as_view(), name='event_dashboard'),
-    # path('rsvp', rsvp, name = 'rsvp'),
     url(r'^rsvp/(?P<event_id>[0-9]+)/$', rsvp, name='rsvp'),
     path('example_event', ExampleEvent.as_view(), name='example_event'),
     path('create_event', CreateEvent.as_view(), name ='create_event'),
